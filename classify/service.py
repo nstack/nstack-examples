cdd362bd763d48edd23e691978a44d635b14e5bf
Roman Cheplyaka <roma@ro-che.info>
2017-03-17 17:29:58 +0200

    Add an AI-based object classifier

---
+++ classify/service.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import nstack
+
+fruits = set(['apple', 'orange', 'banana'])
+vegetables = set(['cabbage', 'carrot', 'potato'])
+chaps = set(['leo', 'rafe', 'roman', 'mandeep', 'ed', 'nick'])
+
+def class_of(s):
+    if s in fruits:
+        return "fruit"
+    if s in vegetables:
+        return "vegetable"
+    if s in chaps:
+        return "chap"
+
+class Service(nstack.BaseService):
+    def classify(self, x):
+        return "{} is a {}".format(x, class_of(x.lower()))
+
